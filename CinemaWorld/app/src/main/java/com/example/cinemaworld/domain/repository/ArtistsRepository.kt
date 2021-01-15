319063618ee5172eaf263ed8d6be2f43f0a45699
ivan <vanolincikus@gmail.com>
2021-01-15 17:57:39 +0200

    init

--- CinemaWorld/app/src/main/java/com/example/cinemaworld/domain/repository/ArtistsRepository.kt
+++ CinemaWorld/app/src/main/java/com/example/cinemaworld/domain/repository/ArtistsRepository.kt
@@ -4,5 +4,5 @@ import com.example.cinemaworld.data.model.artist.Artist
 
 interface ArtistsRepository {
     suspend fun getArtist():List<Artist>?
-    suspend fun updateArtists(): List<Artist>
+    suspend fun updateArtists(): List<Artist>?
 }
